0cf013b58ba534cfb0b0f772ff2d4902819ae9c0
maryamtaghizadeh <mtaghizadeh2023@fau.edu>
2023-11-11 17:22:53 -0500

    new: initial heart rate sensing code
    also print rate to the lcd

--- finalembeddedsystem/finalembeddedsystem.ino
+++ finalembeddedsystem/finalembeddedsystem.ino
@@ -2,32 +2,48 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include "MAX30105.h"
+#include "heartRate.h"
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+
+/***************** HEART RATE ********************/
 MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
+/***************** HEART RATE ********************/
 
 void setup()
 {
-  // initialize the LCD
-  lcd.begin();
   Serial.begin(115200);
 
   /***************** LCD ********************/
+  // initialize the LCD
+  lcd.begin();
   // Turn on the blacklight
   lcd.setBacklight((uint8_t)1);
   /***************** LCD ********************/
 
 
-  /***************** Red + IR ********************/
-	// Initialize sensor
-	if (particleSensor.begin() == false) {
-		Serial.println("MAX30102 was not found. Please check wiring/power.");
-		while (1);
-	}
-	particleSensor.setup(); //Configure sensor. Use 6.4mA for LED drive
-  /***************** Red + IR ********************/
+  /***************** HEART RATE ********************/
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  /***************** HEART RATE ********************/
 }
 #include <Keypad.h>
 
@@ -42,8 +58,8 @@ char hexaKeys[ROWS][COLS] = {
 };
 
 char age[3] = "aa";
-char child_pulse_range[16] = "/75-115 BPM";
-char adult_pulse_range[16] = "/60-100 BPM";
+char child_pulse_range[16] = " /75-115 BPM";
+char adult_pulse_range[16] = " /60-100 BPM";
 
 // byte rowPins[ROWS] = {13, 12, 14, 27}; /* connect to the row pinouts of the keypad */
 // byte colPins[COLS] = {26, 25, 33, 32}; /* connect to the column pinouts of the keypad */
@@ -90,19 +106,45 @@ void loop(){
     lcd.setCursor(0,0);
     lcd.print((String) "Age: " + age + "         ");
     lcd.setCursor(0,1);
-    if (String(age).toInt() <  10){
-      lcd.print(child_pulse_range);
-    } else {
-      lcd.print(adult_pulse_range);
-    }
+    lcd.print("Use HR Sensor");
   }
 
   if (CHECK_PULSE = 1){
-    
+    long irValue = particleSensor.getIR();
+
+    if (checkForBeat(irValue) == true)
+    {
+      //We sensed a beat!
+      long delta = millis() - lastBeat;
+      lastBeat = millis();
+
+      beatsPerMinute = 60 / (delta / 1000.0);
+
+      if (beatsPerMinute < 255 && beatsPerMinute > 20)
+      {
+        rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+        rateSpot %= RATE_SIZE; //Wrap variable
+
+        //Take average of readings
+        beatAvg = 0;
+        for (byte x = 0 ; x < RATE_SIZE ; x++)
+          beatAvg += rates[x];
+        beatAvg /= RATE_SIZE;
+      }
+    }
+    lcd.setCursor(0,1);
+    lcd.print((String) beatAvg + child_pulse_range);
+    Serial.print("IR=");
+    Serial.print(irValue);
+    Serial.print(", BPM=");
+    Serial.print(beatsPerMinute);
+    Serial.print(", Avg BPM=");
+    Serial.print(beatAvg);
+
+    if (irValue < 50000)
+      Serial.print(" No finger?");
+
+    Serial.println();
   }
-  Serial.print(" R[");
-	Serial.print(particleSensor.getRed());
-	Serial.print("] IR[");
-	Serial.print(particleSensor.getIR());
-	Serial.println("]");
+  
 }
